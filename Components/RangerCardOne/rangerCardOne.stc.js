a3abcdc0cd7184c56a97e9d9e71191e01caee1a5
ARSENLUPIN <mdoukali007@gmail.com>
2021-09-29 09:20:25 +0100

    OneStep

---
+++ Components/RangerCardOne/rangerCardOne.stc.js
@@ -0,0 +1,32 @@
+import Styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const RangerCardOneStc = Styled.div`
+
+    //overflow: auto;
+    margin: 0;
+    padding-bottom: 1rem;
+
+    .card{
+        border-radius: 1.5rem;
+        box-shadow: 10px 10px 13px; //rgba(0,0,0,.2);
+    }
+    
+    .maarif{
+        padding-bottom: 1rem;
+        height: 90%;
+        //box-shadow: 1rem 1rem rgba(0,0,0,.3);
+    }
+
+    .saafir{
+        padding-bottom: 3rem;
+        height: 90%;
+    }
+
+    .bernosi{
+        padding-bottom: 5rem;
+        height: 90%;
+    }
+`;
+
+export default RangerCardOneStc;
---
+++ Components/shared/Titre/Titre.stc.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const TitreStc = styled.h2`
+    font-size: 30px;
+    font-weight: 700;
+    padding-bottom: 15px;
+    position: relative;
+    color: ${props => props.color || "#000"};
+    
+    `;
